95b6fc3a86bc81e52a125cf5e2bfbca2a3fce84d
Dane Mancuso <danegmancuso@gmail.com>
2020-08-28 22:46:55 +0000

    routes added for users/avatars/badges/session

--- backend/app/controllers/session_controller.rb
+++ backend/app/controllers/session_controller.rb
@@ -1,2 +1,23 @@
 class SessionController < ApplicationController
+
+  def new
+  end
+
+  def create
+    # checks user exists + password is correct
+    if @user = User.authenticate_with_credentials(params[:email], params[:password])
+      # saves user id in browser cookie so user can navigate freely
+      session[:user_id] = @user.id
+      redirect_to '/'
+    else
+      redirect_to '/login'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to '/login'
+  end
+
+
 end
--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,8 +1,32 @@
 class User < ApplicationRecord
+  #gives User model authentication methods via bcrypt
+  has_secure_password 
+
+  validates :password, :presence => true,
+                       :confirmation => true,
+                       :length => {:within => 6..40},
+                       :on => :create
+
+validates :password, :presence => true,
+                     :confirmation => true,
+                     :length => {:within => 6..40},
+                     :on => :update
+
 
   belongs_to :avatar
   has_many :badges
   has_many :favourites
   has_many :quiz_results
+
+  
+  def self.authenticate_with_credentials(email, password)
+    stripped = email.strip
+    user = User.find_by_email(stripped.downcase)
+    if user && user.authenticate(password)
+      return user
+    else
+      return nil
+    end
+  end
 
 end
