274bd08f922da45933e0bb4b5ce1fe123a28108c
Dane Mancuso <danegmancuso@gmail.com>
2020-08-30 05:50:19 +0000

    included action controller helps method

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::API
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  # protect_from_forgery with: :exception
+  
   include ActionController::Helpers
   
-  protect_from_forgery with: :exception
-
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
