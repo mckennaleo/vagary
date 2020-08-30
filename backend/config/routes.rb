5a2d2f3618208574d2a6b1c7ebaa0f73e30f92b1
Dane Mancuso <danegmancuso@gmail.com>
2020-08-30 04:41:44 +0000

    added view markup for user register

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -26,8 +26,7 @@ Rails.application.routes.draw do
     post '/login' => 'sessions#create'
     get '/logout' => 'sessions#destroy'
   
-    # These routes will be for signup. The first renders a form in the browse, the second will 
-    # receive the form and create a user in our database using the data given to us by the user.
+    # These routes will be for signup. The first renders a form in the browser, the second will receive the form and create a user in the database using the data given to us by the user.
     get '/signup' => 'users#new'
     post '/users' => 'users#create'
 
