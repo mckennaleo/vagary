5558e18fbc43a39cd824e63953cd5089902a5978
Dane Mancuso <danegmancuso@gmail.com>
2020-08-31 20:38:43 +0000

    updated globe state to pass props to city component

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import {
   useParams
 } from "react-router-dom";
 import Globe from './components/Globe';
+import CurrentCity from './components/CurrentCity';
 import axios from 'axios';
 import './App.css';
 
@@ -43,21 +44,11 @@ export default function App() {
         </ul>
 
         <Switch>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route path="/register">
-            <Register />
-          </Route>
-          <Route path="/account">
-            <Account />
-          </Route>
-          <Route path="/city"> {/*template literal with city name*/}
-            <City />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+          <Route path="/account" component={Account} />
+          <Route path="/city" component = {City}/> {/*template literal with city name*/}
+          <Route exact path="/" component={Globe} />
         </Switch>
       </div>
     </Router>
@@ -81,41 +72,15 @@ function Account() {
   return <h2>My Account</h2>;
 }
 
-function City() {
-  let match = useRouteMatch();
-
+function City(props) {
+  console.log(props);
+  let city = props.location.state.city
   return (
     <div>
       <h2>City</h2>
-
-      <ul>
-        <li>
-          <Link to={`${match.url}/welcome`}>Welcome</Link>
-        </li>
-        <li>
-          <Link to={`${match.url}/learn`}>
-            Learn
-          </Link>
-        </li>
-        <li>
-          <Link to={`${match.url}/explore`}>
-            Explore
-          </Link>
-        </li>
-      </ul>
-
-      {/* The Topics page has its own <Switch> with more routes
-          that build on the /topics URL path. You can think of the
-          2nd <Route> here as an "index" page for all topics, or
-          the page that is shown when no topic is selected */}
-      <Switch>
-        <Route path={`${match.path}/:experienceId`}>
-          <Experience />
-        </Route>
-        <Route path={match.path}>
-          <h3>Choose an experience!</h3>
-        </Route>
-      </Switch>
+      <CurrentCity 
+      city = {city}  
+      />
     </div>
   );
 }
--- client/src/components/Globe.js
+++ client/src/components/Globe.js
@@ -1,87 +1,138 @@
 import React, { useState } from 'react';
 import ReactGlobe from 'react-globe';
- 
+import { Redirect } from 'react-router-dom';
+
+
+import CurrentCity from './CurrentCity';
+
 // import optional tippy styles for tooltip support
 import 'tippy.js/dist/tippy.css';
 import 'tippy.js/animations/scale.css';
- 
 
-// export default function Globe() {
-//   return <ReactGlobe />
-// }
 
 
 export default function Globe() {
+
+  // on marker hover, show city name
+  function markerTooltipRenderer(marker) {
+    return `${marker.cityName}`;
+  }
+
+  const options = {
+    markerTooltipRenderer
+  };
+
   // support rendering markers with simple data
   const markers = [
     {
       id: 'marker1',
-      city: 'Oaxaca, Mexico',
+      cityName: 'Oaxaca, Mexico',
       color: 'red',
       coordinates: [17.0654200, -96.7236500],
       value: 50,
     },
     {
       id: 'marker2',
-      city: 'Montréal, Canada',
+      cityName: 'Montréal, Canada',
       color: 'red',
       coordinates: [45.501689, -73.567256],
       value: 50,
     },
     {
       id: 'marker3',
-      city: 'Saigon, Vietnam',
+      cityName: 'Saigon, Vietnam',
       color: 'red',
       coordinates: [10.64975, 106.76198],
       value: 50,
     },
     {
       id: 'marker4',
-      city: 'Budapest, Hungary',
+      cityName: 'Budapest, Hungary',
       color: 'red',
       coordinates: [47.49622, 19.04588],
       value: 50,
     },
     {
       id: 'marker5',
-      city: 'Istanbul, Turkey',
+      cityName: 'Istanbul, Turkey',
       color: 'red',
       coordinates: [41.015137, 28.979530],
       value: 50,
     }
   ];
- 
-  // simple and extensive options to configure globe
-  const options = {
-    ambientLightColor: 'red',
-    cameraRotateSpeed: 0.5,
-    focusAnimationDuration: 2000,
-    focusEasingFunction: ['Linear', 'None'],
-    pointLightColor: 'gold',
-    pointLightIntensity: 1.5,
-    globeGlowColor: 'blue',
-    markerTooltipRenderer: marker => `${marker.city} (${marker.value})`,
+
+  const [city, setCity] = useState(null);
+  const [details, setDetails] = useState(null);
+  const [activeGlobe, setActiveGlobe] = useState(true);
+
+  function onClickMarker(marker, markerObject, city) {
+    setCity({
+      type: "CLICK",
+      marker,
+      markerObjectID: markerObject.uuid,
+      pointerCityPosition: { x: city.clientX, y: city.clientY }
+    });
+    setDetails(markerTooltipRenderer(marker));
+  }
+
+  function onDefocus(previousFocus) {
+    setCity({
+      type: "DEFOCUS",
+      previousFocus
+    });
+    setDetails(null);
+  }
+
+  const yesHandler = () => {
+    // console.log(city)
+    setActiveGlobe(false)
   };
- 
-  const [globe, setGlobe] = useState(null);
-  console.log(globe); // captured globe instance with API methods
- 
-  // simple component usage
-  return (
+
+  if (!activeGlobe) {
+    return (
+    < Redirect
+    to = {{
+      pathname: "/city",
+      state: { city: city }
+    }}
+  />)
+}
+// simple component usage
+return (
+  <div>
+    {details && (
+      <div
+        style={{
+          background: "white",
+          position: "absolute",
+          fontSize: 20,
+          bottom: 0,
+          right: 0,
+          padding: 12
+        }}
+      >
+        <p>{details}</p>
+        <p>
+          EVENT: type={city.type}, position=
+            {JSON.stringify(city.pointerCityPosition)})
+          </p>
+        <form>
+          <h4>Would you like to visit {city.marker.cityName}?</h4>
+          <button type="button" onClick={yesHandler}>Yes</button>
+          <button type="button" onClick={onDefocus}>No</button> {/* FIX Should zoom out per onDefocus function...*/}
+
+        </form>
+      </div>
+    )}
+
     <ReactGlobe
       height="100vh"
-      // globeBackgroundTexture="https://your/own/background.jpg"
-      globeCloudsTexture={null}
-      // globeTexture="https://your/own/globe.jpg"
-      initialCoordinates={[1.3521, 103.8198]}
       markers={markers}
       options={options}
-      width="100%"
-      // onClickMarker={(marker, markerObject, event) => console.log(marker, markerObject, event)}
-      // onGetGlobe={setGlobe}
-      // onMouseOutMarker={(marker, markerObject, event) => console.log(marker, markerObject, event)}
-      onGlobeTextureLoaded={() => console.log('globe loaded')}
-      // onMouseOverMarker={(marker, markerObject, event) => console.log(marker, markerObject, event)}
+      width="100vw"
+      onClickMarker={onClickMarker}
+      onDefocus={onDefocus}
     />
-  )
+  </div>
+)
 }
