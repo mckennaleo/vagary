4c062076719ca832db96588ee72b87de2d6ac7c8
Alyssa Beaton Tanguay <alyssa@macbook.local>
2020-08-31 19:01:41 -0400

    City landing page component

---
+++ client/src/components/WelcomeToCity.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ExploreMap from './explore/ExploreMap'
+
+export default function WelcomeToCity() {
+
+  return(
+    <div>
+      <ExploreMap />
+    </div>
+  )
+}
