76dffb13fd524557a3b7d671272b93f429f671c4
NatalieSokolova <nataliesklv@gmail.com>
2020-09-01 18:03:44 +0000

    adds go to learn/explore logi

--- client/src/components/WelcomeToCity.js
+++ client/src/components/WelcomeToCity.js
@@ -1,77 +1,83 @@
-import React, { useState }from 'react';
-import { Redirect } from 'react-router-dom';
-import ExploreMap from './explore/ExploreMap'
-import Explore from './explore/Explore'
-import Learn from './learn/Learn'
-
+import React, { useState } from "react";
+import { Redirect } from "react-router-dom";
+import ExploreMap from "./explore/ExploreMap";
+import Explore from "./explore/Explore";
+import Learn from "./learn/Learn";
 
 export default function WelcomeToCity(props) {
+  const cityParams = [
+    {
+      name: props.city,
+      coordinates: props.coordinates,
+      language: props.language,
+    },
+  ];
 
-  const cityParams = [{
-    name: props.city,
-    coordinates: props.coordinates,
-    language: props.language
-  }]
-
-  const city = props.city
-  const coordinates = props.coordinates
-<<<<<<< HEAD
-  const language = props.language
+  const city = props.city;
+  const coordinates = props.coordinates;
+  const language = props.language;
 
   const [learn, setLearn] = useState(false);
   const [explore, setExplore] = useState(false);
 
   const goToLearn = () => {
     // console.log(city)
-    setLearn(cityParams)
+    setLearn(cityParams);
   };
   const goToExplore = () => {
     // console.log(city)
-    setExplore(cityParams)
+    setExplore(cityParams);
   };
 
   if (learn) {
     return (
-    < Redirect
-    push to = {{
-      pathname: "/learn",
-      state: { learn }
-    }}
-  />)
+      <Redirect
+        push
+        to={{
+          pathname: "/learn",
+          state: { learn },
+        }}
+      />
+    );
   }
 
   if (explore) {
     return (
-    < Redirect
-    push to = {{
-      pathname: "/explore",
-      state: { explore }
-    }}
-  />)
+      <Redirect
+        push
+        to={{
+          pathname: "/explore",
+          state: { explore },
+        }}
+      />
+    );
   }
 
-=======
-  const city_id = props.city_id
+  const city_id = props.city_id;
 
-  console.log("????", props)
->>>>>>> feature/map
+  console.log("????", props);
 
-  {/* need to take in props to get city name, conditional render background image based on city */}
+  {
+    /* need to take in props to get city name, conditional render background image based on city */
+  }
 
-   {/* make onclick listener, set state, redirect needs to be rendered (needs to be returned)  */}
+  {
+    /* make onclick listener, set state, redirect needs to be rendered (needs to be returned)  */
+  }
 
-  return(
+  return (
     <div>
-       {/* Placeholder for now - city background + buttons to redirect to Learn + Explore */}
-<<<<<<< HEAD
-       <button type="button" onClick={goToLearn} >Go to Learn</button>
-       <button type="button" onClick={goToExplore} >Go to Explore</button>
-       {/*<Explore city={city} coordinates={coordinates}/>*/}
-=======
-       <Explore city={city} coordinates={coordinates} city_id={city_id}/>
->>>>>>> feature/map
+      {/* Placeholder for now - city background + buttons to redirect to Learn + Explore */}
+      <button type="button" onClick={goToLearn}>
+        Go to Learn
+      </button>
+      <button type="button" onClick={goToExplore}>
+        Go to Explore
+      </button>
+      {/*<Explore city={city} coordinates={coordinates}/>*/}
+      <Explore city={city} coordinates={coordinates} city_id={city_id} />
     </div>
-  )
+  );
 }
 
 // import { useHistory } from "react-router-dom";
