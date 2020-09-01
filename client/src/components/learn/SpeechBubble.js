db5d43c1e082f40ebebfa813c1dd88c0b2aedf29
Dane Mancuso <danegmancuso@gmail.com>
2020-09-01 23:43:11 +0000

    new file add speechbubble

---
+++ client/src/components/learn/SpeechBubble.js
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
+
+
+export default function SpeechBubble(props) {
+
+  const language = props.language
+  console.log("LANGUAGE?", language)
+
+    
+
+  const [phrases, setPhrases] = useState([])
+
+  // Get translations from database
+  useEffect(() => {
+    axios({
+      method: "GET",
+      url: `/translations`
+    })
+      .then(results => {
+        // console.log(results)
+        setPhrases(results.data);
+      })
+      .catch(err => console.log(err.message));
+  }, []);
+
+
+  const phraseTranslate = (phrase, lang) => {
+    // console.log("button clicked")
+    axios({
+      "method":"GET",
+      "url":"https://nlp-translation.p.rapidapi.com/v1/translate",
+      "headers":{
+      "content-type":"application/octet-stream",
+      "x-rapidapi-host":"nlp-translation.p.rapidapi.com",
+      "x-rapidapi-key":"8678a05bc3msh6229c171ba7298cp1170c3jsn6cd41dde2894",
+      "useQueryString":true
+      },"params":{
+      "from":lang,
+      "to":"en",
+      "text": phrase
+      }
+      })
+      .then((response)=>{
+        console.log(response.data.translated_text.en)
+      })
+      .catch((error)=>{
+        console.log(error)
+      })
+  }
+
+  return (
+    <section>
+      {phrases.map((phrase) => <button type="button" onClick={ () =>{phraseTranslate(phrase.phrase, language)}}>{phrase.phrase}</button>)}
+    </section>
+  )
+}
