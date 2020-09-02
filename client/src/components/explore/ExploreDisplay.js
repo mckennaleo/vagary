c33ef54a272ec068287ac41090fef18d642da9b6
NatalieSokolova <nataliesklv@gmail.com>
2020-09-02 22:38:48 +0000

    debugs player

--- client/src/components/explore/ExploreDisplay.js
+++ client/src/components/explore/ExploreDisplay.js
@@ -1,32 +1,25 @@
 import React, { Component, useState } from "react";
 import makeRequest from "../../hooks/travelApiData";
 import Youtube from "../../hooks/youtubeApiData";
-import ReactPlayer from 'react-player'
+import ReactPlayer from "react-player";
 
 export default function ExploreDisplay(props) {
-  let videoURL = ''
-  console.log("PROPS", props)
+  let videoURL = "";
+  console.log("PROPS", props);
   const [videoId, setVideoId] = useState(null);
   if (props.display !== undefined) {
-    Youtube(props.display.name)
-    .then(id => {
+    Youtube(props.display.name).then((id) => {
       setVideoId(id);
-    videoURL = 'https://www.youtube.com/watch?v=xWodT0rjw0Y'
-    })};
-  console.log("URL", videoURL)
-    return (
-      <article class="explore-display">
-        <img src={props.display && props.display.photo} />
-        <p>{props.display && props.display.name}</p>
-        <div>{props.display && props.display.description}</div>
-        <ReactPlayer 
-          controls
-          url={videoURL}
-        />
-      </article>
-   
-    );
-  
+      videoURL = "https://www.youtube.com/watch?v=xWodT0rjw0Y";
+    });
+  }
+  console.log("URL", videoURL);
+  return (
+    <article class="explore-display">
+      <img src={props.display && props.display.photo} />
+      <p>{props.display && props.display.name}</p>
+      <div>{props.display && props.display.description}</div>
+      <ReactPlayer controls url="https://www.youtube.com/watch?v=xWodT0rjw0Y" />
+    </article>
+  );
 }
-
-
