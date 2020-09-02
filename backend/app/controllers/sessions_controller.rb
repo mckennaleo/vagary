36ae76cf74343b14c35b2389aa4b3f5620c150a8
Dane Mancuso <danegmancuso@gmail.com>
2020-09-02 23:30:06 +0000

    update user controller to use jwt

--- backend/app/controllers/sessions_controller.rb
+++ backend/app/controllers/sessions_controller.rb
@@ -7,16 +7,21 @@ class SessionsController < ApplicationController
     # checks user exists + password is correct
     if @user = User.authenticate_with_credentials(user_params)
       # saves user id in browser cookie so user can navigate freely
-      session[:user_id] = @user.id
-      redirect_to '/'
+      # session[:user_id] = @user.id
+      # redirect_to '/'
+      render json: {
+        token: JsonWebToken.encode(user_id: @user.id),
+        email: @user.email
+      }
     else
-      redirect_to '/login'
+      # redirect_to '/login'
+      head :unauthorized
     end
   end
 
   def destroy
-    session[:user_id] = nil
-    redirect_to '/login'
+    # session[:user_id] = nil
+    # redirect_to '/login'
   end
 
   private
