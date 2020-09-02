136f5bd2c3e48a277faf907801470056dfccd5b5
Dane Mancuso <danegmancuso@gmail.com>
2020-09-02 21:36:07 +0000

    fill object state on radio button click with relevant info

--- client/src/components/learn/TranslationQuiz.js
+++ client/src/components/learn/TranslationQuiz.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios'
 import { getTranslationQuestionsByCityId } from '../helpers/selectors';
 import FormControl from '@material-ui/core/FormControl';
+import Button from '@material-ui/core/Button';
 import FormLabel from '@material-ui/core/FormLabel';
 import RadioGroup from '@material-ui/core/RadioGroup';
 import Radio from '@material-ui/core/Radio';
@@ -15,11 +16,11 @@ export default function TranslationQuiz(props) {
   const city = props.location.state.translationQuiz[0].name
 
   const [questions, setQuestions] = useState([])
+  const [chosenAnswer, setChosenAnswer] = useState({});
 
-  const [value, setValue] = useState(null);
-
-  const handleChange = (event) => {
-    setValue(event.target.value);
+  const handleChange = (questionId, answer) => {
+    // debugger
+    setChosenAnswer({...chosenAnswer, [questionId]: answer})
   };
 
   // Get translations from database
@@ -36,7 +37,7 @@ export default function TranslationQuiz(props) {
   }, []);
 
 
-  console.log("did it work", questions)
+  console.log(chosenAnswer)
 
 // correct_answer: "Xin chào", incorrect_answer_1
   return (
@@ -45,14 +46,20 @@ export default function TranslationQuiz(props) {
       {questions.map((question) => {
         return (<FormControl component="fieldset">
             <FormLabel component="legend">{question.question}</FormLabel>
-            <RadioGroup aria-label="gender" name="gender1" value={value} onChange={handleChange}>
-              <FormControlLabel value="answer-1" control={<Radio />} label={question.correct_answer} />
-              <FormControlLabel value="answer-2" control={<Radio />} label={question.incorrect_answer_1} />
-              <FormControlLabel value="answer-3" control={<Radio />} label={question.incorrect_answer_2} />
-              <FormControlLabel value="answer-4" control={<Radio />} label={question.incorrect_answer_3} />
+            <RadioGroup aria-label="gender" name="gender1" >
+              <FormControlLabel value="answer-1" control={<Radio />} chosenAnswer={chosenAnswer} onChange={() => handleChange(question.id, question.correct_answer)} label={question.correct_answer} />
+              <FormControlLabel value="answer-2" control={<Radio />} chosenAnswer={chosenAnswer} onChange={() => handleChange(question.id, question.incorrect_answer_1)} label={question.incorrect_answer_1} />
+              <FormControlLabel value="answer-3" control={<Radio />} chosenAnswer={chosenAnswer} onChange={() => handleChange(question.id, question.incorrect_answer_2)} label={question.incorrect_answer_2} />
+              <FormControlLabel value="answer-4" control={<Radio />} chosenAnswer={chosenAnswer} onChange={() => handleChange(question.id, question.incorrect_answer_3)} label={question.incorrect_answer_3} />
             </RadioGroup>
-          </FormControl>)
+          </FormControl>
+          )
     })}
+    <FormControl>
+     <Button type="submit" variant="outlined" color="primary" className=''>
+          Submit
+      </Button>
+    </FormControl>
     </section>
 
   )
