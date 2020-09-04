fad29b057e08b79df63cd13416ce5e0216f086c1
NatalieSokolova <nataliesklv@gmail.com>
2020-09-04 21:18:48 +0000

    wraps playlist retrieving logic in useEffect

--- client/src/components/spotify/Spotify.js
+++ client/src/components/spotify/Spotify.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import SpotifyPlayer from "react-spotify-player";
 import getCity from "../../hooks/cityData.js";
 
@@ -6,33 +6,34 @@ export default function Spotify(props) {
   const size = "compact";
   const view = "coverart"; // or 'coverart'
   const theme = "black"; // or 'white'
-
-  const cityClicked = props.city;
   let newUri;
 
   // spaceSounds playlist
   let uri = "spotify:playlist:5NbleROaHyKOZDwJEPm7f5?si=FgpZbIBMTKOlTUjBi5zv-w";
 
-  if (cityClicked) {
-    console.log("cityClicked Name: ", cityClicked.marker.cityName);
-    const getNewUri = getCity()
-      .then((result) => {
-        newUri = result
-          .filter((city) => city.name === cityClicked.marker.cityName)
-          .map((city) => {
-            return (uri = city.playlist);
-          });
-        console.log("RESULT FROM GETCITY:", result);
-        console.log("NEW URI: ", newUri[0]);
-        uri = newUri[0];
-        return uri;
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-    //uri = getNewUri;
-  }
+  useEffect(() => {
+    const cityClicked = props.city;
+    if (cityClicked) {
+      console.log("cityClicked Name: ", cityClicked.marker.cityName);
+      const getNewUri = getCity()
+        .then((result) => {
+          newUri = result
+            .filter((city) => city.name === cityClicked.marker.cityName)
+            .map((city) => {
+              return (uri = city.playlist);
+            });
+          console.log("RESULT FROM GETCITY:", result);
+          console.log("NEW URI: ", newUri[0]);
+          uri = newUri[0];
 
+          console.log("FINAL URI: ", uri);
+          return uri;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  }, [props.city]);
   return (
     <SpotifyPlayer
       class="spotify"
